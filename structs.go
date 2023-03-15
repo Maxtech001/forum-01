f71f47a7ff13a3ee9488e4127833db0c09d0d73e
Margus Aid <margus.axx@gmail.com>
2023-03-15 10:31:20 +0200

    structs

--- structs.go
+++ structs.go
@@ -6,3 +6,34 @@ type user struct {
 	Email    string
 	Password string
 }
+
+type post struct {
+	Id       int
+	User_id  int
+	Title    string
+	Time     string
+	Content  string
+	Comments []comment
+	Likes    int
+	Dislikes int
+	Tags     []tag
+}
+
+type comment struct {
+	Id       int
+	User_id  string
+	Time     string
+	Content  string
+	Likes    int
+	Dislikes int
+}
+
+type tag struct {
+	Id   int
+	Name string
+}
+
+type mainpage struct {
+	Username string
+	Posts    []post
+}
